85227f3abe076d5a2ce52da8da14a3a52e8f2ae0
AleMax <alemaxsng@gmail.com>
2019-10-24 22:48:33 +0200

    first steps of the vox format

---
+++ src/alemax/ByteHandler.java
@@ -0,0 +1,35 @@
+package alemax;
+
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+
+public class ByteHandler {
+	
+	public static int getInt32(byte[] bytes) {
+		if(bytes.length == 4) {
+			return ByteBuffer.wrap(bytes).order(ByteOrder.LITTLE_ENDIAN).getInt();
+		}
+		return 0;
+	}
+	
+	//Actually useless...
+	public static int getInt8(byte[] bytes) {
+		if(bytes.length == 1) {
+			return bytes[0];
+		}
+		return 0;
+	}
+	
+	public static String getString(byte[] bytes) {
+		return new String(bytes);
+	}
+	
+	public static byte[] getSubArray(byte[] bytes, int index, int count) {
+		byte[] returnBytes = new byte[count];
+		for(int i = 0; i < count; i++) {
+			returnBytes[i] = bytes[index + i];
+		}
+		return returnBytes;
+	}
+	
+}
--- src/alemax/Main.java
+++ src/alemax/Main.java
@@ -1,15 +1,16 @@
 package alemax;
 
+import vox.VoxString;
+
 public class Main {
 
 	public static void main(String[] args) {
 		FileHandler handler = new FileHandler();
-		byte[] array = handler.readVoxFile("vox/test.vox");
-		if(array != null) {
-			System.out.println("success, i guess");
-		} else {
-			System.out.println("nah");
-		}
+		//byte[] bytes = handler.readVoxFile("vox/test.vox");
+		byte[] bytes = { 6, 0, 0, 0, (byte)'A', (byte)'l', (byte)'e', (byte)'M', (byte)'a', (byte)'x'};
+		VoxString string = VoxString.create(bytes, 0);
+		
+		System.out.println(string.string);
 	}
 
 }
