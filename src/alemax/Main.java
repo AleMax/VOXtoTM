18e42e55bb2422ee9f7e9a8d51b7a9eacd7875da
AleMax <alemaxsng@gmail.com>
2019-10-25 20:26:25 +0200

    Revert "Revert "Added rotation converting code""
    This reverts commit 8a6c43b8bc66749517c05dd5a0ecaa265a55ddd9.

--- src/alemax/Main.java
+++ src/alemax/Main.java
@@ -1,17 +1,30 @@
 package alemax;
 
-import alemax.vox.VoxChunkMain;
-import alemax.vox.VoxDict;
-import alemax.vox.VoxString;
+import alemax.vox.VoxRotation;
 
 public class Main {
 
 	public static void main(String[] args) {
+		/*
 		FileHandler handler = new FileHandler();
-		byte[] bytes = handler.readVoxFile("ice2.vox");
+		byte[] bytes = handler.readVoxFile("boxNoRotation.vox");
 		
 		VoxChunkMain main = new VoxChunkMain(bytes, 8);
 		
+
+		short _r = (1 << 0) | (2 << 2) | (0 << 4) | (1 << 5) | (1 << 6);
+		
+		System.out.println(_r);
+		*/
+	
+		VoxRotation rot = new VoxRotation((byte) 113);
+		
+		System.out.println(rot.rotMatrix.getRow(0)[0] + "\t" + rot.rotMatrix.getRow(0)[1] + "\t" + rot.rotMatrix.getRow(0)[2]);
+		System.out.println(rot.rotMatrix.getRow(1)[0] + "\t" + rot.rotMatrix.getRow(1)[1] + "\t" + rot.rotMatrix.getRow(1)[2]);
+		System.out.println(rot.rotMatrix.getRow(2)[0] + "\t" + rot.rotMatrix.getRow(2)[1] + "\t" + rot.rotMatrix.getRow(2)[2]);
+		
 	}
+	
+
 
 }
